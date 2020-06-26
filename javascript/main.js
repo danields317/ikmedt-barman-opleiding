f87833a44b3ca2b0b1974aece170e8b6e40ca601
Daniel <danields317@gmail.com>
2020-06-26 13:52:04 +0200

    Fixed issues created by merge

--- javascript/main.js
+++ javascript/main.js
@@ -10,30 +10,39 @@ const recipes = {
   martini: ["martini"],
   vodka_martini: ["vodka", "martini"],
   gin: ["gin"],
-  vodka_tonic: ["vodka", "tonic"]
+  vodka_tonic: ["vodka", "tonic"],
+  beer: ["beer", "beer", "beer"]
 }
-let recipeOptions = Object.keys(recipes)
+let recipeOptions = Object.keys(recipes);
 let handStatus = handStatusEnum.EMPTY;
 let selectedLiquor = "js--done";
-let liquorColor = "#ffffff";
 let glassContent = [];
-let currentOrder = recipes[recipeOptions.pop()]
-let customerPositions = ["7 0 -1", "5 0 0", "3 0 -2", "9 0 -1", "7 0 1", "5 0 -2", "3 0 -3"]
+let currentOrder = recipes[getRandomRecipe()]
+let customerPositions = ["3 0 2", "5 0 2", "7 0 3", "7 0 1", "9 0 4","9 0 2", "7 0 -1", "5 0 0", "3 0 -2", "9 0 -1", "7 0 1", "5 0 -2", "3 0 -3"]
+const maxCustomers = customerPositions.length
+let customerAmount = 0;
 let currentCustomer;
 let orderBubble;
+let score = 0;
+let liquorColor;
 
 window.onload = () => {
   const sceneEl = document.querySelector('a-scene');
   const camera = document.querySelector('a-camera');
   const buttons = sceneEl.querySelectorAll(".button");
   const coasters = sceneEl.querySelectorAll(".js--coaster");
+  const barkruk = sceneEl.querySelectorAll('.js--barkruk');
   const bell = sceneEl.querySelector("#bell");
   let taps = sceneEl.querySelectorAll(".tap");
   let glass = sceneEl.querySelector('#js--glass');
   let bulbs = sceneEl.querySelectorAll(".bulb");
+  let beertap = document.querySelector("#beertap");
+  let beertapping = document.querySelector("#beertapping");
+  let rig = sceneEl.querySelector('#rig');
 
   currentCustomer = createCustomer(currentOrder);
   sceneEl.appendChild(currentCustomer);
+
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(event){
@@ -44,7 +53,7 @@ window.onload = () => {
   for (let i = 0; i < taps.length; i++) {
     taps[i].addEventListener("click", function(event){
       let newliquorColor = "white";
-      if (glassContent.length < 3) {
+      if (glassContent.length < 3 && taps[i].parentNode.getAttribute("id") == selectedLiquor) {
         switch (selectedLiquor) {
           case "js--gin":
             glassContent.push("gin");
@@ -62,23 +71,41 @@ window.onload = () => {
             glassContent.push("tonic")
             newliquorColor = "#ffff75"
             break;
+          case "js--beer":
+            console.log("hoi");
+            // currentCustomer.setAttribute("animation", {property: "position", to: customerPositions.pop(), dur: "5000", easing: "easeOutQuad"})
+            beertap.setAttribute("visible",false);
+            beertapping.setAttribute("visible",true);
+            if(glassContent.length == 2) {
+              glassContent.push("beer");
+              newliquorColor = "#ffffff"
+              setTimeout(() => {
+                beertap.setAttribute("visible",true);
+                beertapping.setAttribute("visible",false);
+              },1000)
+              break;
+            }
+            glassContent.push("beer")
+            newliquorColor = "#ffc533"
+            setTimeout(() => {
+              beertap.setAttribute("visible",true);
+              beertapping.setAttribute("visible",false);
+            },1000)
+            break;
           default:
             return;
             break;
         }
-        const size = 0.08
-        const offsetY = (-0.1 + (0.08 * (glassContent.length - 1))).toString()
-        const blend = blendColors(liquorColor, newliquorColor, 0.5)
-        glass.appendChild(createLiquor(size, offsetY, blend))
-        liquorColor = blend;
+        const size = (0.08 * glassContent.length).toString();
+        const offsetY = (-0.1 + (0.05 * (glassContent.length - 1))).toString()
+        glass.appendChild(createLiquor(size, offsetY, newliquorColor));
+        liquorColor = newliquorColor;
       }
     })
   }
 
   bell.addEventListener("click", function(event){
     if (selectedLiquor == "js--done") {
-      console.log(glassContent);
-      console.log(currentOrder);
       let correct = true;
       for (var i = 0; i < glassContent.length; i++) {
         if (currentOrder.includes(glassContent[i])) {
@@ -95,6 +122,7 @@ window.onload = () => {
         let customerGlass = createGlass(0.2, 1.2, 0.25);
         orderBubble.setAttribute("value", ":)")
         currentCustomer.appendChild(customerGlass)
+        score++;
       } else {
         orderBubble.setAttribute("value", ">:(")
       }
@@ -107,10 +135,14 @@ window.onload = () => {
         handStatus = handStatusEnum.HOLDGLASS;
       })
       sceneEl.appendChild(glass)
-      currentOrder = recipes[recipeOptions.pop()]
       currentCustomer.setAttribute("animation", {property: "position", to: customerPositions.pop(), dur: "5000", easing: "easeOutQuad"})
-      currentCustomer = createCustomer(currentOrder)
-      sceneEl.appendChild(currentCustomer)
+      if (customerAmount < maxCustomers) {
+        currentOrder = recipes[getRandomRecipe()]
+        currentCustomer = createCustomer(currentOrder)
+        sceneEl.appendChild(currentCustomer)
+      } else {
+        sceneEl.appendChild(createEndScreen())
+      }
     }
   })
 
@@ -140,6 +172,17 @@ window.onload = () => {
          })
         sceneEl.appendChild(glass);
       }
+    })
+  }
+
+  //click kruk
+  for (let i = 0; i < barkruk.length; i++) {
+    barkruk[i].addEventListener("click", function(event){
+        console.log("x: ", rig.getAttribute('position').x);
+        let x = barkruk[i].getAttribute("position").x
+        let y = barkruk[i].getAttribute("position").y
+        let z = barkruk[i].getAttribute("position").z
+        rig.setAttribute('position', {x: x, y: y - 0.35, z: z})
     })
   }
 
@@ -195,6 +238,7 @@ createCustomer = (orderText) => {
   customer.appendChild(orderBubble)
   customer.setAttribute("animation", {property: "position", to: "10 0 -1.8", dur: "5000", easing: "easeOutQuad"})
   customer.setAttribute("position", "-1 0 7");
+  customerAmount++;
   return customer;
 }
 
@@ -205,11 +249,24 @@ createNewGlass = () => {
   return glass
 }
 
-function blendColors(colorA, colorB, amount) {
-  const [rA, gA, bA] = colorA.match(/\w\w/g).map((c) => parseInt(c, 16));
-  const [rB, gB, bB] = colorB.match(/\w\w/g).map((c) => parseInt(c, 16));
-  const r = Math.round(rA + (rB - rA) * amount).toString(16).padStart(2, '0');
-  const g = Math.round(gA + (gB - gA) * amount).toString(16).padStart(2, '0');
-  const b = Math.round(bA + (bB - bA) * amount).toString(16).padStart(2, '0');
-  return '#' + r + g + b;
+createEndScreen = () => {
+  let endScreen = document.createElement("a-plane");
+  endScreen.object3D.position.set(11, 2.35, -2);
+  endScreen.object3D.rotation.set(THREE.Math.degToRad(0), THREE.Math.degToRad(90), THREE.Math.degToRad(0))
+  endScreen.setAttribute("color", "black");
+  endScreen.setAttribute("width", "2");
+  endScreen.setAttribute("height", "0.5")
+  let scoreBox = document.createElement("a-text");
+  let endScore = "Score: "
+  endScore = endScore.concat(score, "/", maxCustomers.toString());
+  scoreBox.setAttribute("class", "clickable")
+  scoreBox.setAttribute("value", endScore);
+  scoreBox.object3D.position.set(-0.6, 0, 0);
+  endScreen.appendChild(scoreBox);
+  return endScreen;
+}
+
+function getRandomRecipe() {
+  const result = recipeOptions[Math.floor(Math.random()*recipeOptions.length)];
+  return result;
 }
